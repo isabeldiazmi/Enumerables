325756dcd92104989aacaa21843d3a8c558b67c6
codeamac07 <codeamac07@gmail.com>
2015-09-11 14:00:46 -0500

    method that returns odd numbers from an array

--- enumerables.rb
+++ enumerables.rb
@@ -1,9 +1,15 @@
 def print_odd_indexed_integers(arr)
   arr.each_with_index do |k,v| 
     if v % 2 != 0 
-      puts k
+      k
     end
   end
 end
-arr = [0,1,2,3,4,5,6]
-print_odd_indexed_integers(arr)
+
+def odd_integers(arr)
+  arr.select{|i| i.odd?}
+end
+
+arr = [0,4,2,7,4,5,6]
+#puts print_odd_indexed_integers(arr)
+puts odd_integers(arr)
