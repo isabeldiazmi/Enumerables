9bbea40e287319657f87fc3c048ba511564fc716
codeamac07 <codeamac07@gmail.com>
2015-09-11 13:50:27 -0500

    method that prints odd indexed integergs from an array

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,9 @@
-def print
+def print_odd_indexed_integers(arr)
+  arr.each_with_index do |k,v| 
+    if v % 2 != 0 
+      puts k
+    end
+  end
+end
+arr = [0,1,2,3,4,5,6]
+print_odd_indexed_integers(arr)
