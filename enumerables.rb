5db74036a9d6b04e31cc3625476c1d6b09f9e8e9
codeamac07 <codeamac07@gmail.com>
2015-09-11 14:27:55 -0500

    mthods that returns the first element under the limit given, other that returns a bang in the end of a string and another one that sums all the numbers given in the array

--- enumerables.rb
+++ enumerables.rb
@@ -10,6 +10,22 @@ def odd_integers(arr)
   arr.select{|i| i.odd?}
 end
 
+def first_under(arr, lim)
+  arr.find{|i| i < lim}
+end
+
+def add_bang(arr)
+  arr.map{|x| x + "!"}
+end
+
+def sum1(arr)
+  arr.reduce{|x,y| x+y}
+end
+
 arr = [0,4,2,7,4,5,6]
+#str = ["isabel", "ximena", "sofia", "adriana"]
 #puts print_odd_indexed_integers(arr)
-puts odd_integers(arr)
+#puts odd_integers(arr)
+#puts first_under(arr, 7)
+#p add_bang(str)
+puts sum1(arr)
